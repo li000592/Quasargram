c7a92f40d181f1fac7cd7bc8e687b6329547db32
li000592 <li000592@algonquinlive.com>
2020-09-02 15:05:56 -0400

    first upload

---
+++ src/pages/PageCamera.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-page class="constrain q-pa-md constrain-more">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="../assets/bdn2020_photo_jcp_refuge.jpg" alt="">
+    </div>
+    <div class="text-center q-pa-md">
+       <q-btn round color="grey-10" icon="eva-camera" size="lg" />
+    </div>
+    <div class="row justify-center q-ma-md">
+       <q-input v-model="text" class="col col-sm-6" label="Caption" dense/>
+    </div>
+    <div class="row justify-center q-ma-md">
+       <q-input v-model="text" class="col col-sm-6" label="Location" dense>
+          <template v-slot:append>
+          <q-btn round dense flat icon="eva-navigation-2-outline" />
+        </template>
+       </q-input>
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <q-btn unelevated rounded color="primary" label="Post Image" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageCamera'
+}
+</script>
+
+<style lang="sass">
+  .camera-frae
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-page class="constrain q-pa-md">
+
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md  " flat bordered>
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://lh3.googleusercontent.com/proxy/W4CpMPHG79yGEAXBqHRAv6Hxf9pZd5cpMnujq0Vfb4-xz3HGstugxX9a-CTaiWIsGzonH_GUSYdOJbP_fJHwoLFJ3E9vOE2wRlo0DtCYZ3Z3EZ2A6Q73uc5u_PeYKBRxmnJbYlkdKAIUi_6V4sLu91ZCRLqYSHt6Iz3I9Ek">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">VanVleet</q-item-label>
+              <q-item-label caption>
+                {{post.location}}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+          <q-img
+          :src="post.imgUrl">
+          </q-img>
+          <q-card-section>
+            <div>{{post.caption}}</div>
+            <div class="text-caption text-grey">{{filters.niceDate(post.date)}}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://lh3.googleusercontent.com/proxy/W4CpMPHG79yGEAXBqHRAv6Hxf9pZd5cpMnujq0Vfb4-xz3HGstugxX9a-CTaiWIsGzonH_GUSYdOJbP_fJHwoLFJ3E9vOE2wRlo0DtCYZ3Z3EZ2A6Q73uc5u_PeYKBRxmnJbYlkdKAIUi_6V4sLu91ZCRLqYSHt6Iz3I9Ek">
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="text-bold">VanVleet</q-item-label>
+            <q-item-label caption>
+              Raptors
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { date } from 'quasar'
+
+export default {
+  name: 'PageHome',
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Parliament Hill',
+          date: 1598986275263,
+          location: 'Ottawa, Ontario, Canada',
+          imgUrl: 'https://www.c-mw.net/wp-content/uploads/2019/05/Majors-Hill-Park-View-of-Parliament-11-credit-Ottawa-Tourism2-1600x1000.jpg'
+        },
+        {
+          id: 2,
+          caption: 'Parliament Hill2',
+          date: 1598986275263,
+          location: 'Ottawa, Ontario, Canada',
+          imgUrl: 'https://www.c-mw.net/wp-content/uploads/2019/05/Majors-Hill-Park-View-of-Parliament-11-credit-Ottawa-Tourism2-1600x1000.jpg'
+        },
+        {
+          id: 3,
+          caption: 'Parliament Hill3',
+          date: 1598986275263,
+          location: 'Ottawa, Ontario, Canada',
+          imgUrl: 'https://www.c-mw.net/wp-content/uploads/2019/05/Majors-Hill-Park-View-of-Parliament-11-credit-Ottawa-Tourism2-1600x1000.jpg'
+        }
+      ],
+      filters: {
+        niceDate (value) {
+          return date.formatDate(value, 'MMMM D h:mmA')
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
